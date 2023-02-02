bec7f0ddf2b968ed3c9a2fa2d5aa469e752acaae
AVA_yc <92014502+lifeandyou@users.noreply.github.com>
2023-02-02 14:14:02 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ChatGPTBot } from "./chatgpt.js";
 
 // Wechaty instance
 const name = "wechat-puppet-wechat";
-let weChatBot = new Wechaty({
+let weChatBot = Wechaty.build({
   name
 });
 // ChatGPTBot instance
